83636e2eda035aff3411797b7a160eae14ecf0d9
lydiawelham <lydiaewelham@gmail.com>
2021-11-17 15:28:30 +0000

    alt text fix

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+
+    return (
+        <div className="navbar">
+        <a className="link" href="#top"><h4 className="initials">LW</h4></a>
+        <div className="header-links">
+          <a className="App-link" href="#projects"><h4>projects</h4></a>
+          <a className="App-link" href="#languages"><h4>languages</h4></a>
+          <a className="App-link" href="#languages"><h4>about me</h4></a>
+        </div>
+      </div>
+    )
+}
+
+export default NavBar
