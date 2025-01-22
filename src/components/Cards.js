cf1dc7aa3c5c3dc64262f9bdb50ea14311ef1575
Lydia Welham <90731882+lydiawelham@users.noreply.github.com>
2025-01-22 14:07:12 +0800

    project and values cards now wrap with narrow screens; profile-img made to cover

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,38 +10,34 @@ const Cards = () => {
     return (
         <div id="cards">
             <h2>my values</h2>
-                <div className="value-card">
-                    <img className="value-icon" src={proven} alt=""/>
-                    <h3>PROVEN</h3>
-                </div>
-                <div className="value-card">
-                    <img className="value-icon" src={simple} alt=""/>
-                    <h3>SIMPLE</h3>
-                </div>
-                <div className="value-card">
-                    <img className="value-icon" src={expressive} alt=""/>
-                    <h3>EXPRESSIVE</h3>
-                </div>
-                <div className="value-card">
-                    <img className="value-icon" src={kind} alt=""/>
-                    <h3>KIND</h3>
+                <div className="card-container">
+                    <div className="value-card">
+                        <img className="value-icon" src={intuitive} alt=""/>
+                        <h3>INTUITIVE</h3>
+                    </div>
+                    <div className="value-card">
+                        <img className="value-icon" src={proven} alt=""/>
+                        <h3>PROVEN</h3>
+                    </div>
+                    <div className="value-card">
+                        <img className="value-icon" src={simple} alt=""/>
+                        <h3>SIMPLE</h3>
+                    </div>
+                    <div className="value-card">
+                        <img className="value-icon" src={expressive} alt=""/>
+                        <h3>EXPRESSIVE</h3>
+                    </div>
+                    <div className="value-card">
+                        <img className="value-icon" src={kind} alt=""/>
+                        <h3>KIND</h3>
+                    </div>
+                    <div className="value-card">
+                        <img className="value-icon" src={agile} alt=""/>
+                        <h3>AGILE</h3>
+                    </div>
                 </div>
         </div>
     ) 
 }
 
 export default Cards
-
-/* Alas! removing values for space - always the way. 
-
-<div className="card-container">
-                <div className="value-card">
-                    <img className="value-icon" src={intuitive} alt=""/>
-                    <h3>INTUITIVE</h3>
-                </div>
- <div className="value-card">
-                    <img className="value-icon" src={agile} alt=""/>
-                    <h3>AGILE</h3>
-                </div>
-                
-*/
